9ccbd1ed2f073fc38e09e9e44c5a7a15422a47bd
Mirko Dziadzka <mirko.dziadzka@gmail.com>
2013-10-30 21:39:43 +0100

    fix the broken whitespace stuff (remove all tabs)

--- hkdf.py
+++ hkdf.py
@@ -16,28 +16,28 @@ class HKDF(object):
     """
 
     def __init__(self, key, salt = "", digestmod =  SHA512):
-	self.__digest = digestmod
-	self.__digest_len =  self.__digest.digest_size
+        self.__digest = digestmod
+        self.__digest_len =  self.__digest.digest_size
 
-	if not salt:
-	    salt = '\0' * self.__digest_len
+        if not salt:
+            salt = '\0' * self.__digest_len
 
-	self.__expanded_key = HMAC.new(salt, key, digestmod = self.__digest).digest()
+        self.__expanded_key = HMAC.new(salt, key, digestmod = self.__digest).digest()
 
     def extract_key(self, info, length):
-	assert length <=  self.__digest_len * 255
+        assert length <=  self.__digest_len * 255
 
-	# generate key stream, stop when we have enought bytes
+        # generate key stream, stop when we have enought bytes
 
-	keystream = ""
-	key_block = ""
-	block_index = 1
+        keystream = ""
+        key_block = ""
+        block_index = 1
 
-	while len(keystream) < length:
-	    key_block = HMAC.new(self.__expanded_key, key_block + info + chr(block_index), digestmod = self.__digest).digest()
-	    block_index += 1
-	    keystream += key_block
+        while len(keystream) < length:
+            key_block = HMAC.new(self.__expanded_key, key_block + info + chr(block_index), digestmod = self.__digest).digest()
+            block_index += 1
+            keystream += key_block
 
-	return keystream[:length]
+        return keystream[:length]
 
 
