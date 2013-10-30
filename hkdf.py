4cda531703062aa52b033702f46a4bbc1d4b4864
Mirko Dziadzka <mirko.dziadzka@gmail.com>
2013-10-30 21:47:42 +0100

    pep8-tify the code

--- hkdf.py
+++ hkdf.py
@@ -10,22 +10,25 @@ http://tools.ietf.org/html/rfc5869
 from Crypto.Hash import SHA256, SHA512
 from Crypto.Hash import HMAC
 
+
 class HKDF(object):
     """
     Wrapper class for HKDF code
     """
 
-    def __init__(self, key, salt = "", digestmod =  SHA512):
+    def __init__(self, key, salt="", digestmod=SHA512):
         self.__digest = digestmod
-        self.__digest_len =  self.__digest.digest_size
+        self.__digest_len = self.__digest.digest_size
 
         if not salt:
             salt = '\0' * self.__digest_len
 
-        self.__expanded_key = HMAC.new(salt, key, digestmod = self.__digest).digest()
+        self.__expanded_key = HMAC.new(salt,
+                                       key,
+                                       digestmod=self.__digest).digest()
 
     def extract_key(self, info, length):
-        assert length <=  self.__digest_len * 255
+        assert length <= self.__digest_len * 255
 
         # generate key stream, stop when we have enought bytes
 
@@ -34,10 +37,10 @@ class HKDF(object):
         block_index = 1
 
         while len(keystream) < length:
-            key_block = HMAC.new(self.__expanded_key, key_block + info + chr(block_index), digestmod = self.__digest).digest()
+            key_block = HMAC.new(self.__expanded_key,
+                                 key_block + info + chr(block_index),
+                                 digestmod=self.__digest).digest()
             block_index += 1
             keystream += key_block
 
         return keystream[:length]
-
-
