1176f1c687c2c3f1068fdc7b1b3636f81a52348d
Mirko Dziadzka <mirko.dziadzka@gmail.com>
2013-11-14 23:37:47 +0100

    complete unittest from rfc

--- test_hkdf.py
+++ test_hkdf.py
@@ -55,6 +55,33 @@ class RFC5869Tests(unittest.TestCase):
         OKM = HKDF(IKM, salt=salt, digestmod=digestmod).extract_key(info, 42)
         assert OKM == "085a01ea1b10f36933068b56efa5ad81a4f14b822f5b091568a9cdd4f155fda2c22e422478d305f3f896".decode("hex")
 
+    def test_rfc5869_Test_Case_5(self):
+        digestmod = SHA1
+        IKM = "000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f202122232425262728292a2b2c2d2e2f303132333435363738393a3b3c3d3e3f404142434445464748494a4b4c4d4e4f".decode("hex")
+        salt = "606162636465666768696a6b6c6d6e6f707172737475767778797a7b7c7d7e7f808182838485868788898a8b8c8d8e8f909192939495969798999a9b9c9d9e9fa0a1a2a3a4a5a6a7a8a9aaabacadaeaf".decode("hex")
+        info = "b0b1b2b3b4b5b6b7b8b9babbbcbdbebfc0c1c2c3c4c5c6c7c8c9cacbcccdcecfd0d1d2d3d4d5d6d7d8d9dadbdcdddedfe0e1e2e3e4e5e6e7e8e9eaebecedeeeff0f1f2f3f4f5f6f7f8f9fafbfcfdfeff".decode("hex")
+
+        OKM = HKDF(IKM, salt=salt, digestmod=digestmod).extract_key(info, 82)
+        assert OKM == "0bd770a74d1160f7c9f12cd5912a06ebff6adcae899d92191fe4305673ba2ffe8fa3f1a4e5ad79f3f334b3b202b2173c486ea37ce3d397ed034c7f9dfeb15c5e927336d0441f4c4300e2cff0d0900b52d3b4".decode("hex")
+
+    def test_rfc5869_Test_Case_6(self):
+        digestmod = SHA1
+        IKM = "0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b".decode("hex")
+        salt = "".decode("hex")
+        info = "".decode("hex")
+
+        OKM = HKDF(IKM, salt=salt, digestmod=digestmod).extract_key(info, 42)
+        assert OKM == "0ac1af7002b3d761d1e55298da9d0506b9ae52057220a306e07b6b87e8df21d0ea00033de03984d34918".decode("hex")
+
+    def test_rfc5869_Test_Case_7(self):
+        digestmod = SHA1
+        IKM = "0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c".decode("hex")
+        # salt not provided
+        info = "".decode("hex")
+
+        OKM = HKDF(IKM, digestmod=digestmod).extract_key(info, 42)
+        assert OKM == "2c91117204d745f3500d636a62f64f0ab3bae548aa53d423b0d1f27ebba6f5e5673a081d70cce7acfc48".decode("hex")
+
 
 if __name__ == '__main__':
     unittest.main()
